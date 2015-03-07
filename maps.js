73e7f9b270ac639b5852e1326321a3ad17f0af36
Kalev Roomann-Kurrik <kalevrk@gmail.com>
2015-03-06 23:05:00 -0800

    Implement basic example of using the Google Maps JavaScript API v3.

---
+++ maps.js
@@ -0,0 +1,12 @@
+function initialize() {
+    var mapOptions = {
+        center: {
+            lat: 37.782,
+            lng: -122.411
+        },
+        zoom: 12
+    };
+    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+};
+
+google.maps.event.addDomListener(window, 'load', initialize);
